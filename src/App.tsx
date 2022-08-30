9e155b570eace3b424e801ce59d9535afb695d62
ssabirovo <muhiddinsobirov957@gmail.com>
2022-08-30 16:52:35 +0500

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,7 @@
-import Hex from "./components/hex";
+
 
 function App() {
-  return (
-    <div className='app'>
-      <Hex color='red' onClick={() => console.log("red clicked")} />
-      <Hex color='#efed40' onClick={() => console.log("#efed40 clicked")} />
-    </div>
-  );
+  return <div className="body"></div>;
 }
 
 export default App;
